14fe1634dd1ef86a485c78c7dc940250a5c40971
morga <morganbakelmun@hotmail.com>
2023-12-07 12:14:13 -0500

    Removed unused functions.

--- app/src/main/java/algonquin/cst2335/ddthrowattack/MainActivity.kt
+++ app/src/main/java/algonquin/cst2335/ddthrowattack/MainActivity.kt
@@ -98,17 +98,8 @@ class MainActivity : ComponentActivity() {
         return weight <= maxWeight
     }
 
-//    private fun calculateRangeShort(weight: Int, capacity: Int): Int {
-//        val fraction = capacity/4
-//        var difference = capacity-weight
-//        val remainder = difference%fraction
-//        difference -= remainder
-//        val additionalRange = difference/fraction
-//        return 5 + 5 * additionalRange
-//    }
-
     private fun calculateRangeShort(weight: Int, capacity: Int): Int {
-        var rangeMultiplier: Int =
+        val rangeMultiplier: Int =
             if (weight*4 <= capacity) {
                 4
             } else if (weight*3 <= capacity) {
@@ -120,39 +111,9 @@ class MainActivity : ComponentActivity() {
             }
         return 5 * rangeMultiplier
     }
-    //
-    //20 str + 40 lb + huge = 60 ft
-    //3 str + 5 lb + small = 20 ft
-
-    private fun calculateRangeLong(weight: Int, pdl: Int): Int {
-        val fraction = pdl/8
-        var difference = pdl/2-weight
-        val remainder = difference%fraction
-        difference -= remainder
-        val additionalRange = difference/fraction
-        return 10 + 10 * additionalRange
-    }
 
     private fun calculateRangeLong(shortRange: Int): Int {
         return shortRange * 3
-    }
-
-    private fun calculateDamage(strength: Int, shortRange: Int, longRange: Int): String {
-        var baseStrength = strength
-        if (strength%2 == 1) {--baseStrength}
-        val strengthModifier = ((baseStrength - 10)/2)
-        val isIncrease: Boolean = strengthModifier >= 0
-
-        var shortTrimmed = shortRange%10
-        shortTrimmed = shortRange-shortTrimmed
-        val shortDamageDice = (shortTrimmed / 10)
-
-        var longTrimmed = longRange%10
-        longTrimmed = longRange-longTrimmed
-        val longDamageDice = (longTrimmed / 10)
-        return ("$shortDamageDice" + "d6"+ if (isIncrease) {"+"} else {""} + "$strengthModifier"
-                + "/" +
-                "$longDamageDice" + "d6"+ if (isIncrease) {"+"} else {""} + "$strengthModifier")
     }
 
     private fun calculateDamage(strength: Int, weight: Int): String {
@@ -163,8 +124,5 @@ class MainActivity : ComponentActivity() {
 
         val dice = weight/50 + 1
         return ("$dice" + "d6"+ if (isIncrease) {"+"} else {""} + "$strengthModifier")
-        //200 = 8d10
-        // 45 = 3d10
-        // 70 = 4d10
     }
 }
